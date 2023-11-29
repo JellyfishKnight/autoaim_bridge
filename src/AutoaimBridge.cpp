aff2c2a6328572af1406c27f38ac3f646a689b55
JellyfishKnight <1002984726@qq.com>
2023-11-29 19:46:43 +0800

    debug tested

--- src/AutoaimBridge.cpp
+++ src/AutoaimBridge.cpp
@@ -11,6 +11,7 @@
 
 
 #include "AutoaimBridge.hpp"
+#include "Packets.hpp"
 
 
 namespace helios_cv {
@@ -141,7 +142,7 @@ void AutoaimBridge::send_callback(autoaim_interfaces::msg::Target::SharedPtr msg
     } else if (msg->id[0] == 'b') {
         send_packet_.id = 9;
     } else {
-        RCLCPP_WARN(logger_, "unknown id!");
+        RCLCPP_WARN(logger_, "unknown id! %s",  msg->id.c_str());
     }
     send_packet_.armors_num = msg->armors_num;
     send_packet_.x = msg->position.x;
